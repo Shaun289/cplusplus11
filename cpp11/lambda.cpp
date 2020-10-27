194d562fa6ae9c328296b7864ac9750a2a623601
sjpark <loblue@gmail.com>
2020-10-28 02:03:02 +0900

    Feat: cpp11 lambda

---
+++ cpp11/lambda.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+// Refs. : https://modoocode.com/196
+
+int main()
+{
+    int my_mod = 8;
+    vector<int> in = { 264,226,264525,26,26,25};
+    vector<int> out;
+
+    out.resize(in.size());
+
+    transform(in.begin(), in.end(), out.begin(),
+        [my_mod](int v) -> int { return v % my_mod; });
+
+    for (auto& x: out) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
