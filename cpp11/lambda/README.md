b8509ec22635706332edfacb081c4c05c43dd115
sjpark <loblue@gmail.com>
2020-10-30 11:01:23 +0900

    Feat: cpp11 lambda #3

--- cpp11/lambda/README.md
+++ cpp11/lambda/README.md
@@ -6,13 +6,22 @@
 [my_mod](int v_) ->int {return v_ % my_mod;}
 ```
 
-- 람다함수의 문법은 위와 같습니다. 자바스크립트 해보신분들은 많이 해보셨을듯
+- 람다함수의 문법은 위와 같습니다. 자바스크립트 해보신분들은 많이 해보셨을듯.. 그냥 문법이죠 뭐 별거있나요.
   - my_mode : introducer 개시자, 외부 변수를 넣으면 이를 람다함수 내부에서 쓸수 있게 해줌
   - int v_ : parameters
   - ->int : return type
   - {} : statement
 - introducer 만 제외하고는 익숙합니다.
 - 기본적으로 introducer는 read-only 입니다. 다음과 같이 하면 컴파일 단계에서 에러가 발생하죠.
+
+## Capture
+- 람다함수 밖의 변수에 접근하기 위한 방법
+  1. [&]() {/* */} : Call by reference
+  2. [&, a, b]() {/* */} : a, b 만 call by value
+  3. [=]() {/* */} : Call by value
+  4. [=, &a, &b]() {/* */} : a, b만 call by reference
+  5. [x, &y, &z]() {/* */} : x는 call by value. y, z는 call by reference
+- call by value의 값을 수정하려할 경우 compile시 에러가 발생합니다.
 
 ```cpp
 lambda.cpp:15:24: error: assignment of read-only variable ‘my_mod’
--- cpp11/lambda/lambda.cpp
+++ cpp11/lambda/lambda.cpp
@@ -6,13 +6,46 @@ using namespace std;
 
 // Refs. : https://modoocode.com/196
 
+int testCapture()
+{
+    int total = 0;
+    vector<int> v = {1,2,3,4};
+
+    // call by reference
+    for_each (v.begin(), v.end(), [&total](int i) { ++total;});
+    cout << "total = " << total << endl;
+
+    // call by value. compile error. total is read-only
+    // for_each (v.begin(), v.end(), [total](int i) { ++total;});
+    // cout << "total = " << total << endl;
+
+}
+
+template <typename T>
+void fill(vector<int>& v, T done)
+{
+    int i = 0;
+    while (!done()) {
+        v.push_back(i++);
+    }
+}
+
+void testCloser()
+{
+    vector<int> v;
+
+    fill(v, [&]() -> bool { return v.size() >= 8;});
+
+    for_each(v.begin(), v.end(), [](int i) { cout << i << " ";});
+    cout << endl;
+}
+
+
 int main()
 {
     int my_mod = 8;
     vector<int> in = { 264,226,264525,26,26,25};
     vector<int> out;
-
-    //[my_mod]() {my_mod -= 4; }(); // Compile error
 
     out.resize(in.size());
 
@@ -23,6 +56,9 @@ int main()
         cout << x << " ";
     }
     cout << endl;
+
+    testCapture();
+    testCloser();
 
     return 0;
 }
