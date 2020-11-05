7454cc34a2ca45c95df8cf706599fe9d66daf13b
sjpark <loblue@gmail.com>
2020-11-05 10:55:13 +0900

    Feat: c++11 rvalue #2

--- cpp11/rvalue/README.md
+++ cpp11/rvalue/README.md
@@ -3,3 +3,12 @@
 
 ## Copy Elision 복사 생략
 - 컴파일러 레벨에서 복사를 생략해버리는 경우를 Copy Elision(복사생략) 이라고 함.
+
+## nullptr
+- NULL = 0 이므로 0과 null pointer를 구분할 수 없음
+- c++11에서 nullptr 이 추가되었고 확실히 구분됨
+- 왜 여기에서 나왔는지 모르겠지만 좋네
+
+## rvalue
+- int a = 3 의 3 처럼 주소값을 취할 수 없는 값을 우측값(rvalue)라고 부름
+- testCopyElision() 이 코드로는 이동생성자가 호출되는 경우를 확인할 수 없네요. 다른 강의를 찾아봐야할듯 합니다.
--- cpp11/rvalue/rvalue.cpp
+++ cpp11/rvalue/rvalue.cpp
@@ -8,13 +8,24 @@ class A {
     public:
     A(int data) : data_(data) { cout << "일반 생성자 호출!" << endl;};
     A(const A& a) : data_(a.data_) { cout << "복사 생성자 호출!" << endl;};
+    A(A&& a) : data_(a.data_) { cout << "이동 생성자 호출!" << endl; };
+    A operator+(const A& a) { data_ += a.data_; return *this;};
+    int data() { return data_;};
 };
 
 void testCopyElision()
 {
+    cout << "a" << endl;
     A a(1);
+    cout << "b" << endl;
     A b(a);
+    cout << "c" << endl;
     A c(A(2));
+
+    cout << "d" << endl;
+    A d = a + b;
+    cout << "d=" << d.data() << endl;
+
 }
 
 int main()
