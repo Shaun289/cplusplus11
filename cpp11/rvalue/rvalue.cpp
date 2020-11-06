c8fb29b242c0e59e965de48197273403a527cb4d
sjpark <loblue@gmail.com>
2020-11-06 11:09:53 +0900

    Feat: c++11 rvalue #3
    - 이해는 한것 같은데 왜 제대로 동작 안하냐...

--- cpp11/rvalue/rvalue.cpp
+++ cpp11/rvalue/rvalue.cpp
@@ -50,10 +50,65 @@ void testRvalueReference()
     printReference(getName());
 }
 
+class ArrayWrapper
+{
+    public:
+    ArrayWrapper(int n) 
+        : _p_vals( new int[n])
+        , _size(n)
+    {
+        cout << "constructor!" << endl;
+    }
+    // copy constructor
+    // 메모리 할당하고 copy 하고 일이 많음. 특히 n 사이즈가 커질수록 부하가 커짐.
+    ArrayWrapper(const ArrayWrapper& other)
+        : _p_vals( new int[other._size])
+        , _size(other._size)
+    {
+        cout << "copy constructor" << endl;
+        for (int i = 0; i < _size; ++i) {
+            _p_vals[i] = other._p_vals[i];
+        }
+    }
+    // move constructor
+    ArrayWrapper(ArrayWrapper&& other)
+        : _p_vals( other._p_vals)
+        , _size(other._size)
+    {
+        cout << "move constructor" << endl;
+        other._p_vals = nullptr;
+        other._size = 0;
+    }
+    ~ArrayWrapper()
+    {
+        delete[] _p_vals;
+    }
+    int size() { return _size;};
+    private:
+    int* _p_vals;
+    int _size;
+};
+
+ArrayWrapper getArrayWrapper()
+{
+    return ArrayWrapper(128);
+}
+
+void testMoveConstructor()
+{
+    ArrayWrapper a(128);
+    cout << "copy!" << endl;
+    ArrayWrapper b(a);
+    cout << "move!" << endl;
+    ArrayWrapper&& c(getArrayWrapper());
+    cout << "moved size=" << c.size() << endl;
+}
+
 int main()
 {
     testCopyElision();
     testRvalueReference();
+    testMoveConstructor();
 
     return 0;
 }
