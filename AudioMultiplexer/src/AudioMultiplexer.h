4c549545020e2a98cd8d94a82e7195261368944e
sjpark <loblue@gmail.com>
2022-03-18 19:47:16 +0900

    Remove 이미 옮긴 AudioMultiplexer 삭제

--- AudioMultiplexer/src/AudioMultiplexer.h
+++
@@ -1,5 +0,0 @@
-
-class AudioMultiPlexer {
-    AudioMultiPlexer() {};
-    ~AudioMultiPlexer() {};
-};
--- AudioMultiplexer/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(int argc, char* argv[])
-{
-    std::cout << __FILE__ << " " << __LINE__ << " " << __func__ << std::endl;
-    return 0;
-}
